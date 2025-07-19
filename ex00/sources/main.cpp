daaa5bf7b278fc23b8b572ce6f7d80dca546dd94
Leonardo Barbosa Sales <leobarbo@c2r8p4.42sp.org.br>
2025-07-19 15:43:42 -0300

    Fixing error messages at ex00

--- ex00/sources/main.cpp
+++ ex00/sources/main.cpp
@@ -2,21 +2,21 @@
 
 int main() {
 	try {
-		Bureaucrat bob("Bob", 151); 
+		Bureaucrat bob("Bob", 151);
 	} catch (std::exception& e) {
-		std::cout << "Erro ao criar Bob: " << e.what() << std::endl;
+		std::cout << "Error creating Bureaucrat: " << e.what() << std::endl;
 	}
 
 	try {
 		Bureaucrat alice("Alice", 2);
 		std::cout << alice << std::endl;
 
-		alice.incrementGrade(); 
+		alice.incrementGrade();
 		std::cout << alice << std::endl;
 
-		alice.incrementGrade(); 
+		alice.incrementGrade();
 	} catch (std::exception& e) {
-		std::cout << "Erro: " << e.what() << std::endl;
+		std::cout << "Error: " << e.what() << std::endl;
 	}
 
 	return 0;
